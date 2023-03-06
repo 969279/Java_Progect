09c9683ea13ffe01a341ced34c7991c57a2a3e9b
Alexandr <guk.zverolis@yandex.ru>
2023-03-06 20:18:51 +0300

    lesson1 homework1

--- src/main/java/lesson1/Task3.java
+++ src/main/java/lesson1/Task3.java
@@ -5,8 +5,30 @@
 
 package lesson1;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        int nums[] = {3, 2, 2, 3, 5, 8, 3, 3, 5, 1};
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int num = in.nextInt();
+
+        shiftArray(nums, num);
+    }
+    private static void shiftArray(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                for (int j = i; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[array.length - 1] = number;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("  " + array[i]);
+        }
     }
 }
+
