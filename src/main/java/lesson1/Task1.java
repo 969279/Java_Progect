3f1601ee37a3466c7c859d18b54c4fca56a73ac6
Alexandr <guk.zverolis@yandex.ru>
2023-03-06 20:04:22 +0300

    init commit

---
+++ src/main/java/lesson1/Task1.java
@@ -0,0 +1,9 @@
+// 1. Задать одномерный массив и найти в нем минимальный и максимальный элементы.
+
+package lesson1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
