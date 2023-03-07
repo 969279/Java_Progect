8587dd61f1693fb610f3ae1ea88cf30bb003281e
Alexandr <guk.zverolis@yandex.ru>
2023-03-07 15:12:51 +0300

    lesson2 init commit

---
+++ src/main/java/lesson2/Task1.java
@@ -0,0 +1,10 @@
+// 1. Напишите метод, который принимает на вход строку (String) и определяет
+// является ли строка палиндромом (возвращает boolean значение).
+
+package lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
