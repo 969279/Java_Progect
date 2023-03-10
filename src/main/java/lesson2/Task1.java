d91935de54c5293554f5b39de164f90c6607b535
Alexandr <guk.zverolis@yandex.ru>
2023-03-10 20:29:42 +0300

    lesson2 Task2

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         System.out.print("Введите строку: ");
         String massageStr = in.next();
--- src/main/java/lesson2/Task2.java
+++ src/main/java/lesson2/Task2.java
@@ -3,11 +3,43 @@
 
 package lesson2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Task2 {
+
     public static void main(String[] args) {
 
+        String text = "TEST";
+        int replay = 100;
+        String PathToSaveFile = "C:\\Users\\Alexandr\\Desktop\\JavaProgect\\" +
+                "JavaProgect\\src\\main\\java\\lesson2\\text.txt";
+        saveToFile(PathToSaveFile, strBuild(text, replay));
+        }
+    private static void saveToFile(String way, String message) {
 
+        String pathToFile = checkPathToFile(way);
+        try(PrintWriter pw = new PrintWriter(pathToFile)) {
+            pw.print(message);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static String checkPathToFile(String path) {
 
+        File dirOfFile = new File(path);
+        if (!dirOfFile.isFile()) {
+            throw new RuntimeException("Файл для сохранения данных не найден!");
+        }
+        return path;
+    }
+    private static String strBuild(String word, int size) {
 
+        StringBuilder stringWord = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            stringWord.append(word).append(" ");
+        }
+        return stringWord.toString();
     }
 }
