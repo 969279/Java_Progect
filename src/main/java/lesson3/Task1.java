c8301fc61f3c66e2869796fa49eb05e63df8d81c
Alexandr <guk.zverolis@yandex.ru>
2023-03-14 19:39:30 +0300

    lesson3 result commit

--- src/main/java/lesson3/Task1.java
+++ src/main/java/lesson3/Task1.java
@@ -1,9 +1,25 @@
-// Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)
+// Пусть дан произвольный список целых чисел,
+// удалить из него четные числа (в языке уже есть что-то готовое для этого).
 
 package lesson3;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
 
+        oddArray();
+    }
+
+    private static void oddArray() {
+        Random random = new Random();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            arrayList.add(random.nextInt(30));
+        }
+        System.out.println(arrayList);
+        arrayList.removeIf(i -> (i % 2) == 0);
+        System.out.println(arrayList);
     }
 }
