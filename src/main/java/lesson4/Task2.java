e88cf78527da7c747f2db0096f43d0d7f3ddda86
Alexandr <guk.zverolis@yandex.ru>
2023-03-15 13:27:04 +0300

    lesson4 init commit

---
+++ src/main/java/lesson4/Task2.java
@@ -0,0 +1,10 @@
+//  Пусть дан LinkedList с несколькими элементами.
+//  Реализуйте метод, который вернет “перевернутый” список.
+
+package lesson4;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
