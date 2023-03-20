88409a8e51797320c2bd44ee45b93b5e6cb142bd
Alexandr <guk.zverolis@yandex.ru>
2023-03-20 10:49:53 +0300

    lesson5 init commit

---
+++ src/main/java/lesson5/Task1.java
@@ -0,0 +1,10 @@
+// Реализуйте структуру телефонной книги с помощью HashMap,
+// учитывая, что 1 человек может иметь несколько телефонов.
+
+package lesson5;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
