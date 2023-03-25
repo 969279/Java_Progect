d616fadcbd02dd68e2abe8b8b9451ca1621a5301
Alexandr <guk.zverolis@yandex.ru>
2023-03-25 12:26:08 +0300

    lesson6 Task1 commit

--- src/main/java/lesson6/Laptop.java
+++ src/main/java/lesson6/Laptop.java
@@ -2,21 +2,24 @@ package lesson6;
 
 public class Laptop {
     String name;
-    int IMEI;
     String brand;
-    int memory;
     double display;
     String cpu;
-    int prise;
+    int hdd;
+    int ram;
 
     @Override
     public String toString() {
         return "Ноутбук: " + brand + '\n' + name + display;
-
     }
 
-    public void showInfo() {
-        System.out.println(this);
+    public Laptop(String name, String brand, double display, String cpu, int hdd, int ram) {
+        this.name = name;
+        this.brand = brand;
+        this.display = display;
+        this.cpu = cpu;
+        this.hdd = hdd;
+        this.ram = ram;
     }
 
     public String getName() {
@@ -27,28 +30,12 @@ public class Laptop {
         this.name = name;
     }
 
-    public int getIMEI() {
-        return IMEI;
-    }
-
-    public void setIMEI(int IMEI) {
-        this.IMEI = IMEI;
-    }
-
     public String getBrand() {
         return brand;
     }
 
     public void setBrand(String brand) {
         this.brand = brand;
-    }
-
-    public int getMemory() {
-        return memory;
-    }
-
-    public void setMemory(int memory) {
-        this.memory = memory;
     }
 
     public double getDisplay() {
@@ -67,11 +54,19 @@ public class Laptop {
         this.cpu = cpu;
     }
 
-    public int getPrise() {
-        return prise;
+    public int getHdd() {
+        return hdd;
     }
 
-    public void setPrise(int prise) {
-        this.prise = prise;
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
     }
 }
