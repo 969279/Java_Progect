197a8dba70098a98a891837edc94f044e61f4776
Alexandr <guk.zverolis@yandex.ru>
2023-03-26 18:09:15 +0300

    lesson6 result commit

--- src/main/java/lesson6/Laptop.java
+++ src/main/java/lesson6/Laptop.java
@@ -1,72 +1,53 @@
 package lesson6;
 
 public class Laptop {
-    String name;
     String brand;
-    double display;
+    String name;
+    String display;
     String cpu;
     int hdd;
     int ram;
 
+    public Laptop( String brand, String name, String display, String cpu, int hdd, int ram) {
+        this.brand = brand;
+        this.name = name;
+        this.display = display;
+        this.cpu = cpu;
+        this.hdd = hdd;
+        this.ram = ram;
+    }
     @Override
     public String toString() {
-        return "Ноутбук: " + brand + '\n' + name + display;
+        return  "Ноутбук: " +
+                brand + ", " +
+                name + ", " +
+                "монитор: " + display + ", " +
+                cpu + ", " +
+                hdd + "ГБ, " +
+                ram + "ГБ;";
     }
 
-    public Laptop(String name, String brand, double display, String cpu, int hdd, int ram) {
-        this.name = name;
-        this.brand = brand;
-        this.display = display;
-        this.cpu = cpu;
-        this.hdd = hdd;
-        this.ram = ram;
-    }
+    public String getName() { return name; }
 
-    public String getName() {
-        return name;
-    }
+    public void setName(String name) { this.name = name;  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getBrand() {  return brand;  }
 
-    public String getBrand() {
-        return brand;
-    }
+    public void setBrand(String brand) { this.brand = brand;  }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+    public String getDisplay() { return display;  }
 
-    public double getDisplay() {
-        return display;
-    }
+    public void setDisplay(String display) { this.display = display;  }
 
-    public void setDisplay(double display) {
-        this.display = display;
-    }
+    public String getCpu() { return cpu;  }
 
-    public String getCpu() {
-        return cpu;
-    }
+    public void setCpu(String cpu) { this.cpu = cpu;  }
 
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
+    public int getHdd() { return hdd;  }
 
-    public int getHdd() {
-        return hdd;
-    }
+    public void setHdd(int hdd) { this.hdd = hdd;  }
 
-    public void setHdd(int hdd) {
-        this.hdd = hdd;
-    }
+    public int getRam() { return ram;  }
 
-    public int getRam() {
-        return ram;
-    }
-
-    public void setRam(int ram) {
-        this.ram = ram;
-    }
+    public void setRam(int ram) {  this.ram = ram;  }
 }
